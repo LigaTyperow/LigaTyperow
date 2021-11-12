5c497a3c76d0b8ccd06e44d0c0a5f6af361984f5
Tomasz <lisssu14@gmail.com>
2021-11-12 22:40:03 +0100

    Dodalem tabele ligowa

--- script/app.js
+++ script/app.js
@@ -1,69 +1,113 @@
 import {
-    getData
+    getDataForShedule,
+    getDataForTable
 } from "../script/apiRequest.js";
 import {
     createDOMElement,
     mapListToDOMElements
 } from "../script/DOMActions.js"
 
-let viewElems = {};
-let leagueNameButtons = {};
-let leagueName="";
+const personalToken = "06900aadf8064cdab4775b8b1c19db88";
+let viewElems = {}; //elementy DOM. Kluczem jest id elementu, a zawartoscia faktyczny element 
+let leagueNameButtons = {}; // Przyciski do zmiany ligi
+let leagueName = ""; //Aktualnie wybrana liga
+
 document.addEventListener("DOMContentLoaded", () => {
-  
-
     connectDOMElements();
-
     Object.keys(leagueNameButtons).forEach(leagueName => {
         leagueNameButtons[leagueName].addEventListener("click", setCurrentNameFilter);
     })
-
-    
 })
 
 const connectDOMElements = () => {
-    const listOfIds = Array.from(document.querySelectorAll('[id]')).map(elem => elem.id);
-    const listOfLeagueNames = Array.from(document.querySelectorAll('[data-league-id]')).map(elem => elem.dataset.leagueId);
+    const listOfIds = Array.from(document.querySelectorAll('[id]')).map(elem => elem.id); //pobiera wszystkie elementy posiadajace 'id', wyciaga te id i tworzy z tego tablice idków
+    const listOfLeagueNames = Array.from(document.querySelectorAll('[data-league-id]')).map(elem => elem.dataset.leagueId); //Jak u góry, z tym że szuka po datasecie leagueId
 
     viewElems = mapListToDOMElements(listOfIds, 'id');
     leagueNameButtons = mapListToDOMElements(listOfLeagueNames, 'data-league-id');
 }
 
-const fetchAndDisplayShows = ()=>{
-    viewElems.finishedMatchesList.innerHTML="";
-    viewElems.UpcomingMatchesList.innerHTML="";
-    getData(leagueName).then(resp => {
-        //document.getElementById('matchday').innerText = resp.filters.dateFrom;
+const fetchAndDisplayData = () => {
+    viewElems.finishedMatchesList.innerHTML = "";
+    viewElems.UpcomingMatchesList.innerHTML = "";
+    const tableBody = document.querySelector('tbody')
+    tableBody.innerHTML = ""
+
+    getDataForShedule(leagueName).then(resp => {
+        switchLeagueName();
         const fullSeasonMatches = resp.matches;
-        console.log(fullSeasonMatches);
         const listOfFinishedMatches = createDOMElement('ul', 'list-group');
         const listOfUpcomingMatches = createDOMElement('ul', 'list-group');
+
         fullSeasonMatches.forEach(match => {
-            const dayOfMatch = match.utcDate.slice(0,10);
+            const dayOfMatch = match.utcDate.slice(0, 10);
             if (match.status === "FINISHED") {
-                const finishedMatchesList = document.getElementById('finishedMatchesList');
-                const singleMatch = createDOMElement('li','list-group-item', `${dayOfMatch} | ${match.awayTeam.name} VS ${match.homeTeam.name} (${match.score.fullTime.awayTeam}:${match.score.fullTime.homeTeam})`);
-                finishedMatchesList.appendChild(listOfFinishedMatches);
+                const singleMatch = createDOMElement('li', 'list-group-item list-group-item-action list-group-item-dark', `${dayOfMatch} ( ${match.score.fullTime.awayTeam}:${match.score.fullTime.homeTeam} ) | ${match.awayTeam.name} VS ${match.homeTeam.name} `);
+                viewElems.finishedMatchesList.appendChild(listOfFinishedMatches);
                 listOfFinishedMatches.appendChild(singleMatch);
             }
-            
+
             if (match.status === "SCHEDULED") {
-                const UpcomingMatchesList = document.getElementById('UpcomingMatchesList');
-                const singleMatch = createDOMElement('li','list-group-item', `${dayOfMatch} | ${match.awayTeam.name} VS ${match.homeTeam.name} `);
-                UpcomingMatchesList.appendChild(listOfUpcomingMatches);
+                const singleMatch = createDOMElement('li', 'list-group-item list-group-item-action list-group-item-dark', `${dayOfMatch} | ${match.awayTeam.name} VS ${match.homeTeam.name} `);
+                viewElems.UpcomingMatchesList.appendChild(listOfUpcomingMatches);
                 listOfUpcomingMatches.appendChild(singleMatch);
             }
+
         });
 
-        // const fullScoreboard = resp.matches;
-        // fullScoreboard.forEach(match => {
-        //     console.log(`${match.homeTeam.name} VS ${match.awayTeam.name}`);
-        // });
+        getDataForTable(leagueName).then(resp => {
+            const standingsTable = resp.standings[0].table;
+            console.log(standingsTable);
+            if (viewElems.leagueTable.style.display = "none") {
+                viewElems.leagueTable.style.display = "initial"
+            } else {
+                viewElems.leagueTable.style.display = "none"
+            }
 
+            standingsTable.forEach(row => {
+                const tr = createDOMElement('tr');
+                const th = createDOMElement('th', "", row.position);
+                th.scope = "row";
+                const teamCrest= createDOMElement('img', "teamCrest", null, row.team.crestUrl);
+                const teamName = createDOMElement('td', "", row.team.name);
+                const matchesPlayed = createDOMElement('td', "", row.playedGames);
+                const pointEarned = createDOMElement('td', "", row.points);
+                const wins = createDOMElement('td', "", row.won);
+                const draws = createDOMElement('td', "", row.draw);
+                const loses = createDOMElement('td', "", row.lost);
+
+
+                tableBody.appendChild(tr);
+                tr.appendChild(th);
+                tr.appendChild(teamCrest);
+                tr.appendChild(teamName);
+                tr.appendChild(matchesPlayed);
+                tr.appendChild(pointEarned);
+                tr.appendChild(wins);
+                tr.appendChild(draws);
+                tr.appendChild(loses);
+            })
+
+        })
     })
+
 }
 
 const setCurrentNameFilter = () => {
     leagueName = event.target.dataset.leagueId;
-    fetchAndDisplayShows();
+    fetchAndDisplayData();
+}
+
+const switchLeagueName = () => {
+    if (leagueName === "PL") {
+        document.getElementById('leagueName').innerText = "Premier League";
+    } else if (leagueName === "BL1") {
+        document.getElementById('leagueName').innerText = "Bundesligi";
+    } else if (leagueName === "SA") {
+        document.getElementById('leagueName').innerText = "Serie A";
+    } else if (leagueName === "PD") {
+        document.getElementById('leagueName').innerText = "La Ligi";
+    } else {
+        document.getElementById('leagueName').innerText = ""
+    }
 }
