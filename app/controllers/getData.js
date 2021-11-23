ef563186a8f17579495b3ee1cd83f280fc9d0c8c
Kubi <jakuub33@wp.pl>
2021-11-23 11:19:24 +0100

    Automatyzacja dodawania danych do bazy

--- app/controllers/getData.js
+++ app/controllers/getData.js
@@ -32,81 +32,57 @@ async function getData(name) {
             headers: headers
         }).then(resp => resp.data.matches)
 
-        let leagueName=""
-        if(name ==="PL") leagueName = "Premier League"
-        else if(name ==="BL1")leagueName = "Bundesliga"
-        else if(name ==="SA")leagueName = "Serie A"
-        else if(name ==="PD")leagueName = "Liga Santander"
-        else leagueName=""
-
-        //pobieranie w petli currentMatchday kazdej ligi
-        // sprawdzamy czy currentMatchday jest taki sam jak w request
-        // if jest taki sam to nie robimy requesta
-        // else (jeslie nie jest taki sam) to robimy requesta i replace i post
-
-        //wczytujemy dane z bd
-        // const bd_nazwa = await Match.find({ leagueName: leagueName, gameweek: currentMatchday });
-            // const bd_nazwa = await Match.find(  )
-            // console.log(bd_nazwa);
-
-        //warunek do ogarniecia        
-        if (true) {
-            await axios.post('http://localhost:80/api/matches', {            
-                finishedMatches,
-                leagueName,
-                upcomingMatches
-            })            
-        }
+        let leagueName = ""
+        if (name === "PL") leagueName = "Premier League"
+        else if (name === "BL1") leagueName = "Bundesliga"
+        else if (name === "SA") leagueName = "Serie A"
+        else if (name === "PD") leagueName = "Liga Santander"
+        else leagueName = ""      
+        
+        await axios.post('http://localhost:80/api/matches', {
+            finishedMatches,
+            leagueName,
+            upcomingMatches
+        })        
     } catch (error) {
         console.error(error);
     }
 }
 
-leagueNames.forEach(name => {
-    getData(name)
-})
+async function ifDataExist() {
+    let currentMatchday = null;
+    try {
+        //pobieramy cyfrę aktualnej kolejki ligi
+        currentMatchday = await axios.get(`http://api.football-data.org/v2/competitions/PL`, {
+            headers: headers
+        }).then(resp => resp.data.currentSeason.currentMatchday)
+
+    } catch (error) {
+        console.log(error);
+    }
+
+    let dbInfo = await Match.findOne({ leagueName: 'Premier League', gameweek: currentMatchday });
+    
+    //Jeśli kolejka obojętnie z jakiej ligi jest inna to mamy nieaktualne dane usuwamy wszystkie mecze i wgrywamy nową
+    if (dbInfo.gameweek != currentMatchday) {
+        console.log(`USUNIETO`);
+        await Match.deleteMany();
+
+        const leagueNames = ["PL", "BL1", "SA", "PD"];
+
+        leagueNames.forEach(name => {
+            getData(name)
+        })
+        console.log(`DODANO DO BAZY`);
+    } else {
+        //jeśli jest taka sama to nie robimy nic
+        console.log(`JUŻ ISTNIEJE`);
+    }
+}
+
+ifDataExist();
+
+// POST do bazy danych ostatniej i przyszłej kolejki
 // leagueNames.forEach(name => {
-//     axios.get(`https://api.football-data.org/v2/competitions/${name}/matches?matchday={}`, {
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'X-Auth-Token': '06900aadf8064cdab4775b8b1c19db88'
-//             }
-//         })
-//         .then(function (resp) {
-//             const matches = resp.data.matches; //mecze w danej kolejce
-//             const leagueName = resp.data.competition.name; //nazwa ligi
-
-//             axios.post('http://localhost:80/api/matches', {
-//                 matches,
-//                 leagueName
-//             })
-//         })
-//         .catch(function (error) {
-//             console.log(error);
-//         })
-//         .then(function () {
-//             // wykona się zawsze, nawet jak będzie błąd
-//         });
-
-// });
-
-// konkretna kolejka
-
-// axios.get('https://api.football-data.org/v2/competitions/PL/matches?matchday=11', {
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'X-Auth-Token': '06900aadf8064cdab4775b8b1c19db88'
-//         }
-//     })
-//     .then(function (resp) {        
-//         const matches = resp.data.matches; //mecze w danej kolejce
-//         const leagueName = resp.data.competition.name; //nazwa ligi
-
-//         axios.post('http://localhost:80/api/matches', {matches, leagueName})
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     })
-//     .then(function () {
-//         // wykona się zawsze, nawet jak będzie błąd
-//     });
+//     getData(name)
+// })
